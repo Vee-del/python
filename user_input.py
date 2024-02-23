b60e7a30c42b59d23f487959332951897727f3b2
Anonymous Pancakes <147178332+Vee-del@users.noreply.github.com>
2024-02-23 17:27:22 -0500

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+Name = input("Enter your name: Winnie Vanessa")
+
+Age = input("Enter your age: 25 years")
+
+Location = input("Enter your location: Nairobi, Kenya")
+
+print( Hello {Winnie Vanessa}, you are {25} years old and live in {Nairobi, Kenya})
